e33909bfeba71db20033872faaddd7527a21ea8d
Alexander Pohl <alex@ahpohl.com>
2023-06-06 22:24:29 +0200

    fix memcpy size argument

--- src/Sunspec-impl.cpp
+++ src/Sunspec-impl.cpp
@@ -9,7 +9,7 @@ template <typename T>
 T Sunspec::ConvertRegister(const uint16_t *tab_reg, const uint16_t &size)
 {
 	T res = 0;
-    memcpy(&res, tab_reg, size);
+    memcpy(&res, tab_reg, sizeof(uint16_t) * size);
     return res;
 }
 
@@ -24,14 +24,14 @@ bool Sunspec::GetIntSf(double &res, const uint16_t &reg_addr, const uint16_t &re
 		return false;
 	}
 	T num = ConvertRegister<T>(tab_reg, reg_size);
-	free(tab_reg);
+	delete tab_reg;
 
 	tab_reg = ReadRegister(sf_addr, sizeof(int16_t));
 	if (!tab_reg) {
 		return false;
 	}
 	int16_t sf = ConvertRegister<int16_t>(tab_reg, sizeof(int16_t));
-	free(tab_reg);
+	delete tab_reg;
 
 	res = static_cast<double>(num) * pow(10, sf);
 
