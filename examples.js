431974ff22877237302b6fb699212dbca426487c
Alexander Pohl (via Travis CI) <alex@ahpohl.com>
2024-10-21 17:20:43 +0000

    Deploy ahpohl/libsunspec to github.com/ahpohl/libsunspec.git:gh-pages

--- examples.js
+++ examples.js
@@ -2,6 +2,7 @@ var examples =
 [
     [ "common.cpp", "common_8cpp-example.html", null ],
     [ "extended.cpp", "extended_8cpp-example.html", null ],
+    [ "fronius-inverter-tcp.cpp", "fronius-inverter-tcp_8cpp-example.html", null ],
     [ "fronius-inverter.cpp", "fronius-inverter_8cpp-example.html", null ],
     [ "fronius-meter.cpp", "fronius-meter_8cpp-example.html", null ],
     [ "immediate.cpp", "immediate_8cpp-example.html", null ],
